7097476e0975987cf6deea3fc1f1503defeb9c65
Albert Huynh <albert.huynh@ibm.com>
2025-10-01 15:42:11 -0400

    delete hack

--- config/initializers/schema_dumper.rb
+++
@@ -1,9 +0,0 @@
-# Strip schema prefix from PostgreSQL extensions in schema.rb
-# This ensures extensions are dumped as "plpgsql" instead of "pg_catalog.plpgsql"
-ActiveSupport.on_load(:active_record) do
-  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(Module.new do
-    def extensions
-      super.map { |ext| ext.gsub(/^pg_catalog\./, "") }
-    end
-  end)
-end
