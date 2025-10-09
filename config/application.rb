0cb94fd03f00d4198da1ba813e9485567f65ac2a
Albert Huynh <albert.huynh@ibm.com>
2025-10-09 10:42:26 -0400

    fix sidekiq web

--- config/application.rb
+++ config/application.rb
@@ -43,5 +43,12 @@ module Media
 
     # Configure ActiveJob to use Sidekiq
     config.active_job.queue_adapter = :sidekiq
+    # This also configures session_options for use below
+    config.session_store :cookie_store, key: "_your_app_session"
+
+    # Required for all session management (regardless of session_store)
+    config.middleware.use ActionDispatch::Cookies
+
+    config.middleware.use config.session_store, config.session_options
   end
 end
