4e6b121a7b993352a18dc20d8a9eed4c5e8f17bf
Albert Huynh <albert.huynh@ibm.com>
2025-12-09 15:27:55 -0500

    add

--- app/controllers/api/v1/async/videos_controller.rb
+++ app/controllers/api/v1/async/videos_controller.rb
@@ -2,6 +2,22 @@ module Api
   module V1
     module Async
       class VideosController < ApiController
+        def show
+          @video = Video.includes(:transcoding_processes).find(params[:id])
+
+          render json: {
+            id: @video.id,
+            external_video_link: @video.external_video_link,
+            transcoding_processes: @video.transcoding_processes.map do |transcoding_process|
+              {
+                id: transcoding_process.id,
+                label: transcoding_process.label,
+                status: transcoding_process.status
+              }
+            end
+          }, status: :ok
+        end
+
         def create
           @video = Video.create!(external_video_link: video_params[:external_video_link])
 
